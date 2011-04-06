2f57b9b5e72b631e558ea7da4cc7b98ec5c75d36
Jason <jason@HAiLu.(none)>
2011-04-06 12:57:16 +0530

    committing after adding multiple client mangment.

--- src/main/java/com/biasedbit/hotpotato/client/manager/DefaultHttpClientManager.java
+++ src/main/java/com/biasedbit/hotpotato/client/manager/DefaultHttpClientManager.java
@@ -6,9 +6,14 @@ import com.biasedbit.hotpotato.request.HttpRequestFuture;
 import com.biasedbit.hotpotato.response.DiscardProcessor;
 import com.biasedbit.hotpotato.response.HttpResponseProcessor;
 import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * DefaultHttpClientManager works as a wrapper for HttpClient so that when using the client only the request need to
@@ -17,25 +22,50 @@ import java.net.URISyntaxException;
  */
 public class DefaultHttpClientManager implements HttpClientManager {
 
+    private static final Logger logger = LoggerFactory.getLogger(DefaultHttpClientManager.class);
     private HttpClientFactory httpClientFactory;
     private HttpResponseProcessor httpResponseProcessor;
     private URI hostUri;
     private HttpClient defaultHttpClient;
 
     /**
+     * This will decide the number of clients that can be used. If the number is set more than one client will be used to send the request
+     */
+    private int numberOfClients = 5;
+    private RoundRobinLogic roundRobinLogic;
+
+
+    public void init() {
+        logger.info("Initiating Client Manager with client pool of [" + numberOfClients + "]");
+        this.defaultHttpClient = httpClientFactory.getClient();
+        defaultHttpClient.init();
+        logger.info("Default client initiated");
+        final ArrayList<HttpClient> httpClients = new ArrayList<HttpClient>(numberOfClients);
+        HttpClient client;
+        for (int i = 0; i < numberOfClients; i++) {
+            client = httpClientFactory.getClient();
+            client.init();
+            httpClients.add(client);
+        }
+        roundRobinLogic = new RoundRobinLogic(httpClients);
+        logger.info("Client manager successfully initiated...");
+    }
+
+    /**
      * Execute the request, before executing the Host URI must be set and if a HttpResponseProcessor is not set
-     * the Responce will be discarded.
+     * the Response will be discarded.
+     *
      * @param request request which was is suppose to be sent.
      * @return
      */
     @Override
     public HttpRequestFuture execute(HttpRequest request) {
+        validateHostUri();
         if (httpResponseProcessor == null) {
-            validateHostUri();
-            return defaultHttpClient.execute(hostUri.getHost(), hostUri.getPort(), request,
+            return execute(hostUri.getHost(), hostUri.getPort(), request,
                     DiscardProcessor.getInstance());
         } else {
-            return defaultHttpClient.execute(hostUri.getHost(), hostUri.getPort(), request, httpResponseProcessor);
+            return execute(hostUri.getHost(), hostUri.getPort(), request, httpResponseProcessor);
         }
     }
 
@@ -47,16 +77,29 @@ public class DefaultHttpClientManager implements HttpClientManager {
 
     @Override
     public HttpRequestFuture execute(HttpRequest request, HttpResponseProcessor httpResponseProcessor) {
+        validateHostUri();
         return execute(hostUri.getHost(), hostUri.getPort(), request, httpResponseProcessor);
     }
 
     @Override
     public HttpRequestFuture execute(String host, int port, HttpRequest request, HttpResponseProcessor httpResponseProcessor) {
-        return defaultHttpClient.execute(host, port, request, httpResponseProcessor);
+        HttpClient client = null;
+        try {
+            client = roundRobinLogic.nextClient();
+        } catch (Throwable e) {
+            logger.error("Error finding the next client : ", e);
+        }
+        if (client != null) {
+            return client.execute(host, port, request, httpResponseProcessor);
+        } else {
+            logger.warn("Error getting the next client, using default client for execution");
+            return defaultHttpClient.execute(host, port, request, httpResponseProcessor);
+        }
     }
 
     /**
      * Return the HttpClient which was created initially.
+     *
      * @return Client Which was created initially.
      */
     @Override
@@ -66,6 +109,7 @@ public class DefaultHttpClientManager implements HttpClientManager {
 
     /**
      * Creates a new Client from the factory and returns it.
+     *
      * @return New Client.
      */
     @Override
@@ -75,13 +119,12 @@ public class DefaultHttpClientManager implements HttpClientManager {
 
     /**
      * Sets the HttpClientFactory and creates one DefaultHttpClient and cashes it.
+     *
      * @param httpClientFactory New HttpClients will be created using the this Factory.
      */
     @Override
     public void setHttpClientFactory(HttpClientFactory httpClientFactory) {
         this.httpClientFactory = httpClientFactory;
-        this.defaultHttpClient = httpClientFactory.getClient();
-        defaultHttpClient.init();
     }
 
     @Override
@@ -104,5 +147,25 @@ public class DefaultHttpClientManager implements HttpClientManager {
 
     public String getHostUri() {
         return hostUri.toString();
+    }
+
+    public void setNumberOfClients(int numberOfClients) {
+        this.numberOfClients = numberOfClients;
+    }
+
+    class RoundRobinLogic {
+
+        private final List<HttpClient> httpClients;
+        private final AtomicLong currentCounter = new AtomicLong(0);
+
+        RoundRobinLogic(List<HttpClient> httpClients) {
+            this.httpClients = httpClients;
+        }
+
+        HttpClient nextClient() {
+            // I know this is not 100% thread safe, but this want be an issue because its just used to share the load and not for anything else
+            currentCounter.compareAndSet(Integer.MAX_VALUE, 0);
+            return httpClients.get((int) currentCounter.getAndIncrement() % httpClients.size());
+        }
     }
 }
